47929c7c01e4de63228d6cb51f9215a31ac5e093
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-03 19:07:08 +0900

    " string test 추가"

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -11,4 +11,19 @@ public class StringTest {
         assertThat(actual).isEqualTo("adc");
         // test commit
     }
+
+    @Test
+    public void split() {
+        String[] values = "1".split(",");
+        assertThat(values).contains("1");
+        values = "1,2".split(",");
+        assertThat(values).containsExactly("1", "2");
+    }
+    @Test
+    public void substring() {
+        String input = "(1,2)";
+        String result = input.substring(1, input.length() - 1);
+        assertThat(result).isEqualTo("1,2");
+    }
+
 }
