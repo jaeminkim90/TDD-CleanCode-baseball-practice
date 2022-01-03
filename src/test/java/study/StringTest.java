ddfe9ce1407de74f0b2ee85ee8031c5dc2071394
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-03 20:23:57 +0900

    0. 학습테스트 실습 - 요구사항 2 테스트 코드 통과

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -15,6 +15,9 @@ public class StringTest {
         // test commit
     }
 
+    // 테스트 요구사항
+    // "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+    // "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
     @Test
     void split() {
         String[] actual1 = "1".split(",");
@@ -23,8 +26,22 @@ public class StringTest {
 
         String[] actual2 = "1,2".split(",");
         assertThat(actual2).contains("1","2");
+        assertThat(actual2).containsExactly("1","2");
         System.out.println(Arrays.toString(actual2));
     }
+
+    // 테스트 요구사항
+    // "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
+    @Test
+    void substring() {
+        String actual = "(1,2)";
+        actual = actual.substring(1);
+        actual = actual.substring(0,3);
+        System.out.println(actual);
+        assertThat(actual).isEqualTo("1,2");
+     }
+
+
 
 
 
