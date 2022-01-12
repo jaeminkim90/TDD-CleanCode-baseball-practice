2fdb78c73edfc372f5d0c52a672f6189d1b01f6d
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-12 16:59:36 +0900

    TEST: 컴퓨터가 랜덤수를 생성하는 creatRandomNumber()의 테스트 코드 수정 완료

--- src/test/java/ReadyGameTest.java
+++ src/test/java/ReadyGameTest.java
@@ -1,7 +1,10 @@
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
+import stringCalculator.StringCalculator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,15 +22,29 @@ class ReadyGameTest {
 
     @Test
     void creatRandomNumberTest() {
-        List<Integer> computerNumber = new ArrayList<>(Arrays.asList(1, 2, 3));
-        assertThat(computerNumber.size()).isEqualTo(3);
+        List<Integer> computerNumber = ReadyGame.creatRandomNumber();
+        SoftAssertions softly = new SoftAssertions();
+
+        assertThat(computerNumber.size()).isEqualTo(3); // 3자리 숫자 확인
+
         for (Integer number : computerNumber) {
-            assertThat(number.getClass().getSimpleName()).isEqualTo("Integer");
+            assertThat(number > 0 && number < 10).isEqualTo(true); // 각 숫자의 범위가 1~10에 해당하는지 확인
+            assertThat(Collections.frequency(computerNumber, number)).isEqualTo(1); // 해당 값이 1개만 존재
         }
     }
 
     @Test
     void splitNumberTest() {
+//        String userNumber = "123";
+//
+//        List<Integer> userNumberList = ReadyGame.
+//                String[]values = StringCalculator.splitString(userString);
+//        // splitString에 문자열 원형을 넣으면 문자 단위로 쪼개진 배열을 반환한다.
+//
+//        assertThat(Arrays.toString(values)).isEqualTo("[1, +, 2, +, 3]");
+//
+//
+//        userNumber.split("");
 
     }
 
